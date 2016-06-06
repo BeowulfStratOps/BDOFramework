1193f7d4364fef20daee4d0b3c8fb5422aff6776
Chris Walker <cw_rocks@hotmail.com>
2016-06-06 17:17:46 +0100

    Added end game trigger

---
+++ bso_framework_v1_2.VR/BSO/safe/safe_note.cpp
@@ -0,0 +1,10 @@
+class CfgNotifications
+{
+	class SafeStart
+	{
+		title = "SafeStart"; // Tile displayed as text on black background. Filled by arguments.
+		iconPicture = "BSO\safe\bso_logo.paa"; // Small icon displayed in left part. Colored by "color", filled by arguments.
+		description = "%1"; // Brief description displayed as structured text. Colored by "color", filled by arguments.
+
+	};
+};	
