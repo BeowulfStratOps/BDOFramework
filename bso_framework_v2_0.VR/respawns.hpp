d3184b05e718165e8951067223dcfc010d2b1527
Keithen <Keithen.Neu@gmail.com>
2020-06-27 12:45:40 +0200

    small config cleanup

--- bso_framework_v2_0.VR/respawns.hpp
+++ bso_framework_v2_0.VR/respawns.hpp
@@ -5,7 +5,7 @@
 /*
 class BSORespawns
 {
-    class west
+    class west // valid sides are west, east, guer, civ. Unused ones can be skipped
     {
         sideTickets = 20; // tickets for this side. default 0.
         personalTickets = 0; // tickets per person. will be consumed first. default 0 -> Entry could be skipped in this case.
@@ -16,7 +16,7 @@ class BSORespawns
 
         onTicketsDepleted = "mission\respawnTicketsDepleted.sqf"; // File will be executed on the server when side tickets run out. This is optional and can be used for custom behaviour. Only parameter is the side.
 
-        class MHQ // Some vehicle spawn point. This is the name it will be referenced by and needs to be unique
+        class MHQ // Some vehicle spawn point. This is the name it will be referenced by; it needs to be unique across all sides
         {
             name = "Mobile HQ"; // displayed name
             object = "b_mhq"; // variable name of a vehicle. Spawn point will become unavailable if vehicle is destroyed
@@ -34,24 +34,7 @@ class BSORespawns
             units[] = {"pilot_1", "pilot_2"}; // only those units will be able to see this spawnpoint
         };
     };
-    class east // other sides. can be skipped. valid sides are west, east, guer, civ
-    {
-        // [...]
-        class opfor_MHQ // spawn names must be different from west
-        {
-            // [...]
-        };
-    };
 };
 */
 
-// available commands
-
-// Enable a spawn point:
-// ["FirstObjective"] call bso_fnc_activatePoint;
-
-// Add 15 tickets to blufor
-// [west, 15] call bis_fnc_respawnTickets;
-
-// Spawn all blufor currently in spectator on the MHQ spawn. Second parameter (false in example) decides if tickets will be consumed
-// ["MHQ", false] call bso_fnc_waveRespawn;
+// For full documentation, see https://beowulfso.com/wiki/BSO_Respawn_System
